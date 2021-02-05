aa6c8ed9bf35d2439f5d026f091db111c78a332b
febby <febbynurfitriyani1208@gmail.com>
2021-02-05 14:16:00 +0700

    function ageInDays

--- js_challenge/static/js/script.js
+++ js_challenge/static/js/script.js
@@ -1,3 +1,14 @@
 function ageInDays() {
     var age = prompt('What year were you born?');
+    var ageInDays1 = (2021 - age) * 365;
+    var h1 = document.createElement('h1');
+    var answer = document.createTextNode('You are ' + ageInDays1 + ' days');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(answer);
+    document.getElementById('flex-box-result').appendChild(h1);
+    console.log(ageInDays1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
 }
